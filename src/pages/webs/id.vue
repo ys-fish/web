35f375a5c2c65b6a223135bd2da405e81a9cbf56
cain <cainyuansheng@gmail.com>
2025-06-20 20:31:13 +0800

    完善网站信息页面

--- src/pages/webs/id.vue
+++ src/pages/webs/id.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="webInfo">
+    <div class="webInfoMock"></div>
     <div class="webInfoHeader">
       <div class="webInfoHeaderBox">
         <div class="webInfoLogo">
@@ -14,8 +15,8 @@
             justify-content: space-between;
           "
         >
-          <h4 class="webInfoTitle">Vue.js</h4>
-          <div class="webInfoDocs">黎明xxw</div>
+          <h4 class="webInfoTitle">黎明起始页</h4>
+          <div class="webInfoDocs">深圳市黎明科技有限公司</div>
           <div class="webInfoTags">
             <div class="webInfoTag">
               <div>4.1</div>
@@ -28,20 +29,72 @@
           </div>
         </div>
       </div>
-      <div class="webInfoText">
-        adadaadadaswqewqeqweqqwewqeqweqwwwwwwwwwwwwwwwwwww
-        qwewqeqweqweqweqw21313122akdkasdkadakdkakdasksakkdakdkasd
-        kaskkdkwqewqekqwkrewqkkxckczxkaskrkqewrkqeqweqweqweqweqweqweqweqweqw
-      </div>
-      <div style="display: flex; align-items: center;margin-top: 10px;gap: 20px;">
-        <v-btn flat color="#154599" > 进入网站 </v-btn>
+      <div class="webInfoText">科技改变世界</div>
+      <div
+        style="display: flex; align-items: center; margin-top: 10px; gap: 20px"
+      >
+        <v-btn flat color="#154599"> 进入网站 </v-btn>
         <v-icon size="16">mdi-share-variant-outline</v-icon>
       </div>
-      <div style="font-size: 12px;margin-top: 5px;">由黎明xxw提供和更新</div>
+      <div style="font-size: 12px; margin-top: 5px">由黎明xxw提供和更新</div>
 
+      <div class="centen">
+        <div class="centenLeft">
+          <div class="couselBox">
+            <div class="couselBoxHeader">
+              <div style="font-weight: 700">屏幕截图</div>
+              <div><v-icon>mdi-chevron-right</v-icon></div>
+            </div>
+            <v-divider></v-divider>
+            <div style="padding: 20px 0px">
+              <v-slide-group show-arrows>
+                <v-slide-group-item v-for="n in 3" :key="n" v-slot="{ toggle }">
+                  <v-img
+                    class="ma-2"
+                    @click="toggle"
+                    width="560px"
+                    height="330px"
+                    cover
+                    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+                  ></v-img>
+                </v-slide-group-item>
+              </v-slide-group>
+            </div>
+          </div>
 
-      <div class="couselBox">
-        <div></div>
+          <div class="couselBox">
+            <div class="couselBoxHeader">
+              <div style="font-weight: 700">描述</div>
+              <div><v-icon>mdi-chevron-right</v-icon></div>
+            </div>
+            <v-divider></v-divider>
+            <div style="padding: 20px 20px;font-size: 14px;">
+              抖音让每一个人看见并连接更大的世界，鼓励表达、沟通和记录，激发创造，丰富人们的精神世界，让生活更美好。
+              抖音电脑版，新功能上线，快来体验！<br /> 【小窗播放，自由切换】
+              视频变小窗，精彩不占屏，学习娱乐两不误；
+              全屏or小窗，随你所想，屏幕空间尽在掌握。 【抖音视频，秒变壁纸】
+              选择你喜欢的视频，设置为桌面壁纸，陪伴一天好心情。
+              还可以点击“看抖音”、“看作者”，一键观看更省心。
+            </div>
+          </div>
+
+            <div class="couselBox">
+            <div class="couselBoxHeader">
+              <div style="font-weight: 700">评分和评价</div>
+              <div><v-icon>mdi-chevron-right</v-icon></div>
+            </div>
+            <v-divider></v-divider>
+            <div style="padding: 20px 20px;font-size: 14px;">
+              抖音让每一个人看见并连接更大的世界，鼓励表达、沟通和记录，激发创造，丰富人们的精神世界，让生活更美好。
+              抖音电脑版，新功能上线，快来体验！<br /> 【小窗播放，自由切换】
+              视频变小窗，精彩不占屏，学习娱乐两不误；
+              全屏or小窗，随你所想，屏幕空间尽在掌握。 【抖音视频，秒变壁纸】
+              选择你喜欢的视频，设置为桌面壁纸，陪伴一天好心情。
+              还可以点击“看抖音”、“看作者”，一键观看更省心。
+            </div>
+          </div>
+        </div>
+        <div class="centenRight"></div>
       </div>
     </div>
   </div>
@@ -50,19 +103,19 @@
 <style lang="scss" scoped>
 .webInfo {
   width: 100%;
-  height: 100vh;
+  height: 100%;
   box-sizing: border-box;
   padding: 15px 25px;
   background: #f9f9f9;
   .webInfoHeader {
-    width: 85%;
+    width: 84%;
     margin: 80px auto;
     .webInfoHeaderBox {
       display: flex;
       gap: 20px;
       .webInfoLogo {
-        width: 100px;
-        height: 100px;
+        width: 86px;
+        height: 86px;
         border-radius: 10px;
         background: white;
         display: flex;
@@ -70,11 +123,13 @@
         justify-content: center;
       }
       .webInfoTitle {
-        font-size: 24px;
+        font-size: 30px;
+        margin: 0;
+        padding: 0;
       }
       .webInfoDocs {
         font-size: 14px;
-        color: blue;
+        color: #1565c0;
       }
     }
     .webInfoTags {
@@ -96,12 +151,40 @@
     }
   }
 
-  .couselBox{
+  .centen {
+    display: flex;
+    gap: 60px;
+    .centenLeft {
+      width: 78%;
+      position: relative;
+    }
+  }
+
+  .webInfoMock {
+    position: fixed;
+    width: 100vw;
+    height: 300px;
+    left: 0;
+    top: 0;
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 128, 0, 0.1) 30%,
+      rgba(0, 128, 0, 0) 70%
+    );
+  }
+
+  .couselBox {
     box-sizing: border-box;
-    padding: 20px;
+    // padding: 20px;
     background: white;
     margin-top: 20px;
     border-radius: 10px;
+    .couselBoxHeader {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 10px 20px;
+    }
   }
 }
 </style>
